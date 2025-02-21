c77c5cd1a74df78892d03ab15cfa40790a73fce0
arjun292619 <j.c.arjunreddy@gmail.com>
2025-02-21 09:22:39 -0800

    employee management front end: add dummy data and render list component

--- employee-management-service/employee-management-frontend/ems-app/src/App.jsx
+++ employee-management-service/employee-management-frontend/ems-app/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import ListEmployee from './components/ListEmployee';
 
 function App() {
   return (
     <>
-      <h1>Employment Management Application</h1>
+      <h1 className="text-center">Employment Management Application</h1>
+      <ListEmployee />
     </>
   );
 }
